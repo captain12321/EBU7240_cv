70c8d4057046cd5096e30421649b5ab91bd7736c
captain1014 <100767823+captain12321@users.noreply.github.com>
2022-12-16 16:24:43 +0800

    Add files via upload

--- ex4b.py
+++ ex4b.py
@@ -7,149 +7,6 @@ import cv2
 import math
 
 
-class SVM:
-    def __init__(self, max_iter=100, kernel='linear'):
-        self.max_iter = max_iter
-        self._kernel = kernel
-
-    #参数初始化
-    def init_args(self, features, labels):
-        self.m, self.n = features.shape
-        self.X = features
-        self.Y = labels
-        self.b = 0.0
-        self.alpha = np.ones(self.m)
-        self.computer_product_matrix()#为了加快训练速度创建一个内积矩阵
-        # 松弛变量
-        self.C = 1.0
-        # 将Ei保存在一个列表里
-        self.create_E()
-
-    #KKT条件判断
-    def judge_KKT(self, i):
-        y_g = self.function_g(i) * self.Y[i]
-        if self.alpha[i] == 0:
-            return y_g >= 1
-        elif 0 < self.alpha[i] < self.C:
-            return y_g == 1
-        else:
-            return y_g <= 1
-
-    #计算内积矩阵#如果数据量较大，可以使用系数矩阵
-    def computer_product_matrix(self):
-        self.product_matrix = np.zeros((self.m,self.m)).astype(float)
-        for i in range(self.m):
-            for j in range(self.m):
-                if self.product_matrix[i][j]==0.0:
-                    self.product_matrix[i][j]=self.product_matrix[j][i]= self.kernel(self.X[i], self.X[j])
-
-    # 核函数
-    def kernel(self, x1, x2):
-        if self._kernel == 'linear':
-            return np.dot(x1,x2)
-        elif self._kernel == 'poly':
-            return (np.dot(x1,x2) + 1) ** 2
-        return 0
-
-    #将Ei保存在一个列表里
-    def create_E(self):
-        self.E=(np.dot((self.alpha * self.Y),self.product_matrix)+self.b)-self.Y
-
-    # 预测函数g(x)
-    def function_g(self, i):
-        return self.b+np.dot((self.alpha * self.Y),self.product_matrix[i])
-
-    #选择变量
-    def select_alpha(self):
-        # 外层循环首先遍历所有满足0<a<C的样本点，检验是否满足KKT
-        index_list = [i for i in range(self.m) if 0 < self.alpha[i] < self.C]
-        # 否则遍历整个训练集
-        non_satisfy_list = [i for i in range(self.m) if i not in index_list]
-        index_list.extend(non_satisfy_list)
-        for i in index_list:
-            if self.judge_KKT(i):
-                continue
-            E1 = self.E[i]
-            # 如果E2是+，选择最小的；如果E2是负的，选择最大的
-            if E1 >= 0:
-                j =np.argmin(self.E)
-            else:
-                j = np.argmax(self.E)
-            return i, j
-
-    #剪切
-    def clip_alpha(self, _alpha, L, H):
-        if _alpha > H:
-            return H
-        elif _alpha < L:
-            return L
-        else:
-            return _alpha
-    #训练函数，使用SMO算法
-    def Train(self, features, labels):
-        self.init_args(features, labels)
-        #SMO算法训练
-        for t in range(self.max_iter):
-            i1, i2 = self.select_alpha()
-
-            # 边界
-            if self.Y[i1] == self.Y[i2]:
-                L = max(0, self.alpha[i1] + self.alpha[i2] - self.C)
-                H = min(self.C, self.alpha[i1] + self.alpha[i2])
-            else:
-                L = max(0, self.alpha[i2] - self.alpha[i1])
-                H = min(self.C, self.C + self.alpha[i2] - self.alpha[i1])
-
-            E1 = self.E[i1]
-            E2 = self.E[i2]
-            # eta=K11+K22-2K12
-            eta = self.kernel(self.X[i1], self.X[i1]) + self.kernel(self.X[i2], self.X[i2]) - 2 * self.kernel(
-                self.X[i1], self.X[i2])
-            if eta <= 0:
-                # print('eta <= 0')
-                continue
-
-            alpha2_new_unc = self.alpha[i2] + self.Y[i2] * (E1 - E2) / eta  # 此处有修改，根据书上应该是E1 - E2，书上130-131页
-            alpha2_new = self.clip_alpha(alpha2_new_unc, L, H)
-
-            alpha1_new = self.alpha[i1] + self.Y[i1] * self.Y[i2] * (self.alpha[i2] - alpha2_new)
-
-            b1_new = -E1 - self.Y[i1] * self.kernel(self.X[i1], self.X[i1]) * (alpha1_new - self.alpha[i1]) - self.Y[
-                i2] * self.kernel(self.X[i2], self.X[i1]) * (alpha2_new - self.alpha[i2]) + self.b
-            b2_new = -E2 - self.Y[i1] * self.kernel(self.X[i1], self.X[i2]) * (alpha1_new - self.alpha[i1]) - self.Y[
-                i2] * self.kernel(self.X[i2], self.X[i2]) * (alpha2_new - self.alpha[i2]) + self.b
-
-            if 0 < alpha1_new < self.C:
-                b_new = b1_new
-            elif 0 < alpha2_new < self.C:
-                b_new = b2_new
-            else:
-                # 选择中点
-                b_new = (b1_new + b2_new) / 2
-
-            # 更新参数
-            self.alpha[i1] = alpha1_new
-            self.alpha[i2] = alpha2_new
-            self.b = b_new
-
-            self.create_E()
-
-    def predict(self, data):
-        r = self.b
-        for i in range(self.m):
-            r += self.alpha[i] * self.Y[i] * self.kernel(data, self.X[i])
-
-        return 1 if r > 0 else 0
-
-    def score(self, X_test, y_test,predict_result):
-        right_count = 0
-        for i in range(len(X_test)):
-            result = self.predict(X_test[i])
-            predict_result.append(result)
-            if result == y_test[i]:
-                right_count += 1
-        return right_count / len(X_test),predict_result
-
 class Classifier(object):
     def __init__(self, filePath):
         self.filePath = filePath
@@ -163,93 +20,67 @@ class Classifier(object):
     def get_data(self):
         TrainData = []
         TestData = []
-        num_in_test = 200  # test image number
+        num_in_test = 1000 # test image number
         data = self.unpickle(self.filePath)
         train = np.reshape(data[b'data'][0:2000,:], (2000, 3, 32 * 32))
         labels = np.reshape(data[b'labels'][0:2000], (2000, 1))
+        num=0
         for i in range(len(labels)):
             if labels[i] != 0:
+                num+=1
                 labels[i] = 1  # 等于1说明不是飞机
+        #print('train: negative_sample:{0} positive_sample:{1}'.format(num, len(labels) - num))
         fileNames = np.reshape(data[b'filenames'][0:2000], (2000, 1))
         datalebels = zip(train, labels, fileNames) # turn it in tuple
         TrainData.extend(datalebels)
         test = np.reshape(data[b'data'][2000:2000+num_in_test,:], (num_in_test, 3, 32 * 32))
         labels = np.reshape(data[b'labels'][2000:2000+num_in_test], (num_in_test, 1))
+        num=0
         for i in range(len(labels)):
             if labels[i] != 0:
+                num+=1
                 labels[i] = 1  # 等于1说明不是飞机
+        #print('test: negative_sample:{0} positive_sample:{1}'.format(num,len(labels)-num))
         fileNames = np.reshape(data[b'filenames'][2000:2000+num_in_test], (num_in_test, 1))
         TestData.extend(zip(test, labels, fileNames))
         print("data read finished!")
         return TrainData, TestData,test
 
-    def get_hog_feat(self, image, stride=8, orientations=8, pixels_per_cell=(8, 8), cells_per_block=(2, 2)):
-        cx, cy = pixels_per_cell
-        bx, by = cells_per_block
-        sx, sy = image.shape
-        n_cellsx = int(np.floor(sx // cx))  # number of cells in x
-        n_cellsy = int(np.floor(sy // cy))  # number of cells in y
-        n_blocksx = (n_cellsx - bx) + 1
-        n_blocksy = (n_cellsy - by) + 1
-        gx = np.zeros((sx, sy), dtype=np.float32)
-        gy = np.zeros((sx, sy), dtype=np.float32)
-        eps = 1e-5
-        grad = np.zeros((sx, sy, 2), dtype=np.float32)
-        for i in range(1, sx-1):
-            for j in range(1, sy-1):
-                gx[i, j] = image[i, j-1] - image[i, j+1]
-                gy[i, j] = image[i+1, j] - image[i-1, j]
-                grad[i, j, 0] = np.arctan(gy[i, j] / (gx[i, j] + eps)) * 180 / math.pi
-                if gx[i, j] < 0:
-                    grad[i, j, 0] += 180
-                grad[i, j, 0] = (grad[i, j, 0] + 360) % 360
-                grad[i, j, 1] = np.sqrt(gy[i, j] ** 2 + gx[i, j] ** 2)
-        normalised_blocks = np.zeros((n_blocksy, n_blocksx, by * bx * orientations))
-        for y in range(n_blocksy):
-            for x in range(n_blocksx):
-                block = grad[y*stride:y*stride+16, x*stride:x*stride+16]
-                hist_block = np.zeros(32, dtype=np.float32)
-                eps = 1e-5
-                for k in range(by):
-                    for m in range(bx):
-                        cell = block[k*8:(k+1)*8, m*8:(m+1)*8]
-                        hist_cell = np.zeros(8, dtype=np.float32)
-                        for i in range(cy):
-                            for j in range(cx):
-                                n = int(cell[i, j, 0] / 45)
-                                hist_cell[n] += cell[i, j, 1]
-                        hist_block[(k * bx + m) * orientations:(k * bx + m + 1) * orientations] = hist_cell[:]
-                normalised_blocks[y, x, :] = hist_block / np.sqrt(hist_block.sum() ** 2 + eps)
-        #print(normalised_blocks.ravel().shape)
-        return normalised_blocks.ravel() #展成1维向量
-
+    def get_hog_feat(self, image):
+        winSize=(32,32)
+        blockSize=(4,4)
+        blockStride=(1,1)
+        cellSize=(2,2)
+        Bin=9
+        hog = cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize, Bin)
+        hist=hog.compute(image,(8,8))
+        return hist.reshape(-1)
     def get_feat(self, TrainData, TestData,a):
         train_feat = []
         test_feat = []
         for data in TestData:
             image = np.reshape(data[0].T, (32, 32, 3))
-
             if a!='1':
-                gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) / 255.
-                fd = self.get_hog_feat(gray)
+                #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) / 255.
+                fd = self.get_hog_feat(image)
             else:
                 img = image.flatten()  # 对图像进行降维操作，方便算法计算
                 fd = img / np.mean(img)  # 归一化，突出特征
             fd = np.concatenate((fd, data[1]))
             test_feat.append(fd)
-        test_feat = np.array(test_feat)
+        test_feat = np.array(test_feat,dtype='float32')
         print("Test features are extracted")
         for data in TrainData:
             image = np.reshape(data[0].T, (32, 32, 3))
             if a!='1':
-                gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) / 255.
-                fd = self.get_hog_feat(gray)
+                fd = self.get_hog_feat(image)
+                #print('ok')
             else:
                 img = image.flatten()  # 对图像进行降维操作，方便算法计算
                 fd = img / np.mean(img)  # 归一化，突出特征
             fd = np.concatenate((fd, data[1]))
             train_feat.append(fd)
-        train_feat = np.array(train_feat)
+        train_feat = np.array(train_feat,dtype='float32')
         print("Train features are extracted")
         return train_feat, test_feat
 
@@ -258,12 +89,24 @@ class Classifier(object):
             name='RGB'
         else:
             name='HOG'
-        clf = SVM(max_iter=200)
+        svm = cv2.ml.SVM_create()  # 创建svm学习模型
+        svm.setType(cv2.ml.SVM_C_SVC)  # 类型为svm分类
+        svm.setKernel(cv2.ml.SVM_LINEAR)  # 设置svm的内核为线性分类器
+        svm.setC(0.01)
+        train_label=[]
         print("Training a Linear SVM Classifier.")
-        clf.Train(train_feat[:, :-1], train_feat[:, -1]) #最后一位是标志
-        predict_result=[]
-        score,predict_result = clf.score(test_feat[:, :-1],test_feat[:,-1],predict_result)
+        for i in range(train_feat.shape[0]):
+            train_label1=[int(train_feat[i,-1])]
+            train_label.append(train_label1)
+        train_label=np.array(train_label,dtype='int')
+        print(train_label.shape)
+        print(train_feat[:,:-1].shape)
 
+        svm.train(train_feat[:, :-1],cv2.ml.ROW_SAMPLE,train_label) #最后一位是标志
+        predict_result=svm.predict(test_feat[:,:-1])
+        predict_result=predict_result[1].flatten()
+        print(predict_result.shape)
+        #score,predict_result = clf.score(test_feat[:, :-1],test_feat[:,-1],predict_result)
         print('This is prediction results:\n')
         for i in range(len(predict_result)):
             print(int(predict_result[i]),end="")
@@ -280,7 +123,7 @@ class Classifier(object):
         for i in range(len(predict_result)):
             if int(predict_result[i]) == int(test_feat[i, -1]):
                 num += 1
-                #print(i,end=" ")
+                print(i,end=" ")
 
                 if predict_result[i]==0 and count_true<5:
                     plane.append(i)
@@ -289,9 +132,8 @@ class Classifier(object):
                     not_plane.append(i)
                     count_false += 1
         print('correct prediction numbers:\n'+str(num))
-        rate = score
         #print(plane)
-        print('The classification accuracy is %f' % rate)
+        print('The classification accuracy is {0}'.format(num/len(predict_result)))
         count=0
         for i in plane:
             count+=1
